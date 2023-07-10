30a28e9bb63697f7b8d94c1cf9b63c1615b333dc
DougCh <dougch@amazon.com>
2023-07-10 21:48:05 +0000

    nix devshell

--- flake.nix
+++ flake.nix
@@ -6,6 +6,21 @@
   outputs = { self, nix, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+        common_packages = [
+          # Integration Deps
+          # We're not including openssl1.1.1 in our package list to avoid confusing cmake.
+          # It will be in the PATH of our devShell for use in tests.
+
+          # Linters/Formatters
+          pkgs.shellcheck
+          pkgs.nixfmt
+          pkgs.go
+
+          # Quality of Life
+          pkgs.findutils
+          pkgs.git
+          pkgs.which
+        ];
       in rec {
         packages.aws-lc = pkgs.stdenv.mkDerivation {
           src = self;
@@ -23,6 +38,13 @@
          (finalAttrs: previousAttrs: {
             doCheck = true;
             });
+        devShells.default = pkgs.mkShell {
+          inherit system;
+          packages = common_packages;
+          shellHook = ''
+            export PS1="[nix awslc] $PS1"
+          '';
+        };
       });
 }
 
