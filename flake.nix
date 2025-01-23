14ebc46073a608dc747b92c62ed2c0c4ce0a6fe5
Doug Chapman <dougch@amazon.com>
2025-01-23 20:56:23 +0000

    version fips package

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        packages.aws-lc-fips = pkgs.stdenv.mkDerivation {
+        packages.aws-lc-fips2022 = pkgs.stdenv.mkDerivation {
           src = self;
           name = "aws-lc-fips";
           inherit system;
