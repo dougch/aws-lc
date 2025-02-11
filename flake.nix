1db6b454ec7fdf3b3b177a60391dd19113c0d577
DougCh <dougch@amazon.com>
2025-02-11 00:13:18 +0000

    chore: build for nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "AWS-LC is a general-purpose cryptographic library";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+  outputs = { self, nix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages.aws-lc-fips = pkgs.stdenv.mkDerivation {
+          hardeningDisable = [ "fortify" ];
+          src = self;
+          name = "aws-lc-fips";
+          inherit system;
+          nativeBuildInputs = [ pkgs.ninja pkgs.cmake pkgs.perl pkgs.go ];
+          cmakeFlags = [
+            "-GNinja"
+            "-DBUILD_SHARED_LIBS=0"
+            "-DCMAKE_BUILD_TYPE=relwithdebinfo"
+            "-DFIPS=1"
+          ];
+          buildPhase = ''
+            # Workaround a bug where the value of $HOME is set to
+            # non-writable /homeless-shelter dir
+            # see https://github.com/NixOS/nix/issues/670
+            export HOME=$(pwd)
+            export GOPROXY="direct"
+          '';
+          checkPhase = ''
+            ninja run_minimal_tests
+          '';
+        };
+        formatter = pkgs.nixfmt;
+        packages.default = packages.aws-lc-fips;
+        packages.aws-lc-fips-test = packages.aws-lc-fips.overrideAttrs
+          (finalAttrs: previousAttrs: { doCheck = true; });
+      });
+}
+
